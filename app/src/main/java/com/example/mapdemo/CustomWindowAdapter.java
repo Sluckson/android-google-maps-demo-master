c353de5d6c63ddfc16f1003874a760696ab93af5
luckson surprice <lucksonsurprice94@gmail.com>
2020-12-20 14:18:38 -0500

    first commit

---
+++ app/src/main/java/com/example/mapdemo/CustomWindowAdapter.java
@@ -0,0 +1,39 @@
+package com.example.mapdemo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class CustomWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    LayoutInflater mInflater;
+
+    public CustomWindowAdapter(LayoutInflater i){
+        mInflater = i;
+    }
+
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        // Getting view from the layout file
+        View v = mInflater.inflate(R.layout.custom_info_window, null);
+        // Populate fields
+        TextView title = (TextView) v.findViewById(R.id.tv_info_window_title);
+        title.setText(marker.getTitle());
+
+        TextView description = (TextView) v.findViewById(R.id.tv_info_window_description);
+        description.setText(marker.getSnippet());
+        // Return info window contents
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+
+        return null;
+    }
+
+
+}
